a3026bfdc1d390cc48fe6da1aa742388c5419b62
Siddhant <115331356+grimsteel@users.noreply.github.com>
2023-03-26 00:50:12 -0500

    Setup admin for parcel, workspaces

--- admin/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-export const app = express();
-app.set("view engine", "pug");
-app.set('views', "./admin/views");
-app.use(express.static("./admin/public"));
-
-app.get("/", (_req, res) => res.render("index.pug"));
